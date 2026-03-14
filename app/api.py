a82ed452cfcef0506cbe1ba23ab4355e6ad1c79f
Zaidddddddd <100534202+Zaidddddddd@users.noreply.github.com>
2026-03-14 15:50:27 +0530

    fixed  parameters

--- app/api.py
+++ app/api.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from routers import metrics,aws
+from routers import metrics , aws
 app = FastAPI(
-    title ="Internal DevOps Utility API",
-    description= "this is an Internal API Utility App for Monitoring metrics,AWS usag,logs",
+    title="Internal DevOps Utility API", 
+    description="this is an Internal API Utility App for Monitoring metrics,AWS usag,logs",
     version="1.1.0",
     doc_url="/docs"
 )
 
 app.include_router(metrics.router)
-app.include_router(aws.router, prefix = "/aws")
+app.include_router(aws.router,prefix ="/aws")
